d0424ca8800833c7f147cfea74e94a9470fa7b18
mariana6665 <mariana_6665@hotmail.com>
2016-12-15 04:14:02 -0300

    cubito arreglado

--- cubito.py
+++ cubito.py
@@ -1,43 +1,40 @@
-###############################
-#Como leer un cubo de datos
-###############################
+import astropy.io.fits as fits
+import matplotlib.pyplot as plt
+import numpy as np
+import os
 
 
-import astropy.io.fits as fits #modulo para leer archivos fits
-import matplotlib.pyplot as plt #modulo para graficar
-import numpy as np #este modulo es para trabajar con matrices como en matlab
-
-#esta funcion les entrega un arreglo con los valores reales del eje correspondiente
-#1 es velocidad, 2 es longirud galactica, 3 es latitud galactica
 def values(h,j):
-	N=h['NAXIS'+str(j)];
-	val=np.zeros(N);
+	N = h['NAXIS'+str(j)];
+	val = np.zeros(N);
 	for i in range(0,N):
-		val[i] = (i+1-float(h['CRPIX'+str(j)]))*float(h['CDELT'+str(j)]) + float(h['CRVAL'+str(j)]);
+		val[i] = (i + 1 - float(h['CRPIX' + str(j)]))*float(h['CDELT' + str(j)]) + float(h['CRVAL' + str(j)]);
 	return val;
 
-cubo	= fits.open("Cubo_de_datos.fits") #abrir objeto cubo de datos
-data 	= cubo[0].data #extraer matriz de datos
-header	= cubo[0].header #extraer el header del archivo fits
-#Estos seran los tres arreglos con los valores reales de los tres ejes del cubo
-velocidad=values(header,1)
-longitud=values(header,2)
-latitud=values(header,3)
-j=1000
+cubo = fits.open("Cubo_de_datos.fits")
+data = cubo[0].data
+header = cubo[0].header
+
+#Arreglos con los valores de Velocidades, Longitudes y Latitudes
+vel = values(header,1)
+lon = values(header,2)
+lat = values(header,3)
+j = 1000
 
 v = np.zeros(385)
-velocidades = np.zeros(385)
-velocidadesabs = np.zeros(385)
 radios = np.zeros(385)
+velocidades = np.zeros(385)
+Vabs = np.zeros(385)
 b = np.zeros(385)
 c = np.zeros(33)
 latitudes = np.zeros(385)
+
 indice = 1000
 
 
-for k in range(0,len(longitud)):
+for k in range(0,len(lon)):
         print k
-        for h in range(0,len(latitud)):
+        for h in range(0,len(lat)):
                 j=1000
                 for i,tem in enumerate(data[h][k][:]):
                         if i<305 and tem >= 0.5 and data[h][k][i+1]>=tem:
@@ -50,55 +47,69 @@ for k in range(0,len(longitud)):
                         break
         b[k]=p
 
-for m in range (0,len(longitud)):
+for m in range (0,len(lon)):
         print m
-        velocidades[m]=velocidad[v[m]]
+        velocidades[m] = vel[v[m]]
 
-for q in range (0,len(longitud)):
+for q in range (0,len(lon)):
         print q
-        latitudes[q]=latitud[b[q]]
-for vel in range (0,len(longitud)):
-        velocidadesabs[vel]=abs(velocidades[vel])
-for rad in range (0,len(longitud)):
-        radios[rad]=-8.5*np.sin(3.14*longitud[rad]/180)
-for rad in range (0,len(longitud)):
-        radios[rad]=-8.5*np.sin(3.14*longitud[rad]/180)
+        latitudes[q] = lat[b[q]]
+for vel in range (0,len(lon)):
+        Vabs[vel] = abs(velocidades[vel])
+for rad in range (0,len(lon)):
+        radios[rad] = -8.5*np.sin(np.pi*lon[rad]/180)
+for rad in range (0,len(lon)):
+        radios[rad] = -8.5*np.sin(np.pi*lon[rad]/180)
 
-vrot = velocidadesabs+radios*220/8.5
-altura = abs(8.5*np.cos(longitud*3.14/180)*np.sin(latitudes*3.14/180))
-distancia = abs(8.5*np.cos(longitud*3.14/180))
+vrot = Vabs + radios*220/8.5
+Z = abs(8.5*np.cos(lon*np.pi/180)*np.sin(latitudes*np.pi/180))
+distancia = abs(8.5*np.cos(lon*np.pi/180))
 archivo = open('masa.dat','w')
-import os
+
 for i in range(len(vrot)):
 	archivo.write(str(radios[i]) + ' ' + str(vrot[i]) +'\n')
 archivo.close()
 
-#Modelos de masa:
+#Calcula la velocidad de rotacion promedio
+suma = 0
+for i in range(len(vrot)):
+	suma = suma + vrot[i]
+	i = i + 1
+print "velocidad de rotacion promedio:"
+print suma/len(vrot)
 
+
+#Modelos de distribucion de masa
+#modelo1: masa puntual
 M1 = 2.95423*(10**21)
-masa1 = np.sqrt(6.67384*(10**(-11))*M1/radios)/1000
+M1r = np.sqrt(6.67384*(10**(-11))*M1/radios)/1000
 
-p2 = 4.74717*(10**19)
-masa2 = np.sqrt(6.67384*(10**(-11))*3.14*radios*p2)/1000
+#modelo2: disco uniforme
+sigma2 = 4.74717*(10**19)
+M2r = np.sqrt(6.67384*(10**(-11))*np.pi*radios*sigma2)/1000
 
-p3 = 6.83774*(10**17)
-masa3 = np.sqrt(6.67384*(10**(-11))*4*3.14*(radios**2)*p3/3)/1000
+#modelo3: esfera uniforme
+rho3 = 6.83774*(10**17)
+M3r = np.sqrt(6.67384*(10**(-11))*4*np.pi*(radios**2)*rho3/3)/1000
 
+#modelo4: masa puntual + disco uniforme
 M4 = 1.11313*(10**21)
-p4 = 3.11928*(10**19)
-masa4 = np.sqrt(6.67384*(10**(-11))*(M4+3.14*(radios**2)*p4)/radios)/1000
+sigma4 = 3.11928*(10**19)
+M4r = np.sqrt(6.67384*(10**(-11))*(M4 + np.pi*(radios**2)*sigma4)/radios)/1000
 
+#masa puntual + esfera uniforme
 M5 = 1.61088*(10**21)
-p5 = 3.23028*(10**18)
-masa5 = np.sqrt(6.67384*(10**(-11))*(M5+4*3.14*(radios**3)*p5/3)/radios)/1000
+rho5 = 3.23028*(10**18)
+M5r = np.sqrt(6.67384*(10**(-11))*(M5 + 4*np.pi*(radios**3)*rho5/3)/radios)/1000
 
-#graficos
-radioss=radios/8.5
-xlargo=[0,8]
-xancho=[220,220]
-fig=plt.figure()
-"""
-plt.plot(radioss,vrot,'m.')
+
+R = radios/8.5
+xlargo = [0,8]
+xancho = [220,220]
+fig = plt.figure()
+
+#Grafico velocidad de rotacion
+plt.plot(R, vrot, 'm.')
 plt.xlabel("Radio/Ro")
 plt.ylabel("Velocidad de rotacion [km/s]")
 plt.title("Velocidad de rotacion en funcion del Radio")
@@ -106,14 +117,8 @@ plt.axis([0.1, 0.9, 180, 260])
 plt.show()
 fig.savefig('vel_rot.png')
 
-plt.plot(radioss,velocidadesabs,'m.')
-plt.xlabel("Radio/Ro")
-plt.ylabel("V_LSR[km/s]")
-plt.title("Velocidad LSR en funcion del Radio")
-plt.show()
-fig.savefig('vel_lsr.png')
-
-plt.plot(distancia,altura,'m-')
+#Grafico Corrugacion del plano
+plt.plot(distancia, Z, 'm-')
 plt.axis([4, 8.5, 0, 0.20])
 plt.xlabel("Distancia del centro galactico [kpc]")
 plt.ylabel("Z [kpc]")
@@ -121,21 +126,11 @@ plt.title("Corrugacion del plano")
 plt.show()
 fig.savefig('corrugacion.png')
 
-plt.plot(radioss,vrot,'m.',radioss,masa1,'r-',radioss,masa2,'b-',radioss,masa3,'c-',radioss,masa4,'g-',radioss,masa5,'k-')
+#Grafico Modelos de Distribucion de Masa
+plt.plot(R, vrot, 'm.', R, M1r, 'r-', R, M2r, 'b-', R, M3r, 'c-', R, M4r, 'g-', R, M5r, 'k-')
 plt.title("Modelos de masa")
 plt.ylabel("Velocidad de rotacion [km/s]")
 plt.xlabel("Radio/Ro")
-
 plt.axis([0.1, 0.9, 159, 300])
 plt.show()
-
-
-
 fig.savefig('masas.png')
-"""
-suma = 0
-for i in range(len(vrot)):
-	suma = suma + vrot[i]
-	i = i + 1
-print "velocidad de rotacion promedio:"
-print suma/len(vrot)
